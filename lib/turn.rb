dfcc7d4171754c07883f485a11f7f19b9d7919b7
Jason Calvo <jason.calvo7@gmail.com>
2020-04-22 16:29:13 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def input_to_index(input)
+  input.to_i - 1 
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board,index)
+  end
+
+def move(board, index, name = "X")
+  board[index] = name
+end
+
+def position_taken?(board, index)
+   board[index] != " "
+  end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
+  
